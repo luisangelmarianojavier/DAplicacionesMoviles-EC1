34290c556e02dfdc8627483fc74b9f719c693a00
Luis Ramos <luismeramosr@gmail.com>
2021-04-19 12:39:23 -0500

    pre merge commit

---
+++ app/src/main/res/layout/farmacias/ListadoEnfermeros.java
@@ -0,0 +1,16 @@
+package com.idat.farmacias;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListadoEnfermeros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_enfermeros);
+    }
+
+}
