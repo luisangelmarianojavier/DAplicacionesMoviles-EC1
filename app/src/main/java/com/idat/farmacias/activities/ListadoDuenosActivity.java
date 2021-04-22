c2f8ff4581fd0ecd4c9f35a2dde6dbc5997b1598
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-04-21 20:07:22 -0500

    se agrego listado sin problemas

---
+++ app/src/main/java/com/idat/farmacias/activities/ListadoDuenosActivity.java
@@ -0,0 +1,16 @@
+package com.idat.farmacias.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.idat.farmacias.R;
+
+public class ListadoDuenosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_duenos);
+    }
+}
